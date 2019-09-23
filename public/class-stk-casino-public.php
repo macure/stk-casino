fccc3470259cd99ffb454d1710d9c9bd1f9bf45c
Marko Ćurčić <marko.curcic.gir@gmail.com>
2019-09-23 16:15:11 +0200

    Casino feature image background

--- public/class-stk-casino-public.php
+++ public/class-stk-casino-public.php
@@ -617,6 +617,89 @@ class Stk_Casino_Public
                 'description' => '',
             ));
 
+            acf_add_local_field_group(array(
+                'key' => 'group_5d88cb84c5aae',
+                'title' => 'Casino Style',
+                'fields' => array(
+                    array(
+                        'key' => 'field_5d88cba839c40',
+                        'label' => 'Featured image background',
+                        'name' => 'featured_image_background',
+                        'type' => 'color_picker',
+                        'instructions' => 'Choose the background colour for featured image.',
+                        'required' => 0,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'default_value' => '#d7dfe2',
+                    ),
+                ),
+                'location' => array(
+                    array(
+                        array(
+                            'param' => 'post_type',
+                            'operator' => '==',
+                            'value' => 'casino',
+                        ),
+                    ),
+                ),
+                'menu_order' => 0,
+                'position' => 'side',
+                'style' => 'default',
+                'label_placement' => 'top',
+                'instruction_placement' => 'label',
+                'hide_on_screen' => '',
+                'active' => true,
+                'description' => '',
+            ));
+
+            acf_add_local_field_group(array(
+                'key' => 'group_5cc85d89cbd29',
+                'title' => 'Page Data',
+                'fields' => array(
+                    array(
+                        'key' => 'field_5cc85da0fb39c',
+                        'label' => 'Content intro',
+                        'name' => 'content_intro',
+                        'type' => 'wysiwyg',
+                        'instructions' => '',
+                        'required' => 0,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'default_value' => '',
+                        'tabs' => 'all',
+                        'toolbar' => 'full',
+                        'media_upload' => 1,
+                        'delay' => 0,
+                    ),
+                ),
+                'location' => array(
+                    array(
+                        array(
+                            'param' => 'page_type',
+                            'operator' => '==',
+                            'value' => 'front_page',
+                        ),
+                    ),
+                ),
+                'menu_order' => 0,
+                'position' => 'acf_after_title',
+                'style' => 'default',
+                'label_placement' => 'top',
+                'instruction_placement' => 'label',
+                'hide_on_screen' => '',
+                'active' => true,
+                'description' => '',
+            ));
+
+
         endif;
     }
 }
