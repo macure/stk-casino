34f2c2622498f58d74c8f4dbc36c9561a7759920
Marko Ćurčić <marko.curcic.gir@gmail.com>
2019-10-04 12:01:43 +0200

    Added Casino style

--- public/class-stk-casino-public.php
+++ public/class-stk-casino-public.php
@@ -680,5 +680,48 @@ class Stk_Casino_Public
             ));
 
         endif;
+
+        if (function_exists('acf_add_local_field_group')) :
+
+            acf_add_local_field_group(array(
+                'key' => 'group_5d88cb84c5aae',
+                'title' => 'Casino Style',
+                'fields' => array(
+                    array(
+                        'key' => 'field_5d88cba839c40',
+                        'label' => 'Featured image background',
+                        'name' => 'featured_image_background',
+                        'type' => 'color_picker',
+                        'instructions' => 'Choose the background colour for featured image.',
+                        'required' => 0,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'default_value' => '#d7dfe2',
+                    ),
+                ),
+                'location' => array(
+                    array(
+                        array(
+                            'param' => 'post_type',
+                            'operator' => '==',
+                            'value' => 'casino',
+                        ),
+                    ),
+                ),
+                'menu_order' => 0,
+                'position' => 'side',
+                'style' => 'default',
+                'label_placement' => 'top',
+                'instruction_placement' => 'label',
+                'hide_on_screen' => '',
+                'active' => true,
+                'description' => '',
+            ));
+
+        endif;
     }
 }
