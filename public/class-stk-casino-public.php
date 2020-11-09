acdd55ace2ba580d71d9e06fbcae1b3003b2c901
Marko Ćurčić <marko.curcic.gir@gmail.com>
2020-11-09 16:56:25 +0100

    Added slots custom post type

--- public/class-stk-casino-public.php
+++ public/class-stk-casino-public.php
@@ -142,6 +142,40 @@ class Stk_Casino_Public
         );
 
         register_post_type("casino", $args);
+
+        /**
+         * Post Type: Slots.
+         */
+
+        $labels = [
+            "name" => __("Slots", "minisite"),
+            "singular_name" => __("Slot", "minisite"),
+        ];
+
+        $args = [
+            "label" => __("Slots", "minisite"),
+            "labels" => $labels,
+            "description" => "",
+            "public" => true,
+            "publicly_queryable" => true,
+            "show_ui" => true,
+            "show_in_rest" => true,
+            "rest_base" => "",
+            "rest_controller_class" => "WP_REST_Posts_Controller",
+            "has_archive" => false,
+            "show_in_menu" => true,
+            "show_in_nav_menus" => true,
+            "delete_with_user" => false,
+            "exclude_from_search" => false,
+            "capability_type" => "post",
+            "map_meta_cap" => true,
+            "hierarchical" => false,
+            "rewrite" => ["slug" => "slot", "with_front" => true],
+            "query_var" => true,
+            "supports" => ["title", "editor", "thumbnail", "custom-fields"],
+        ];
+
+        register_post_type("slot", $args);
     }
 
     /**
