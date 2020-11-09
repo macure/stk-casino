978c8d547fd59733418ef081e98c42c41b3cb5b2
Marko Ćurčić <marko.curcic.gir@gmail.com>
2020-11-09 16:47:47 +0100

    Cache custom fields

--- includes/class-stk-casino.php
+++ includes/class-stk-casino.php
@@ -199,7 +199,7 @@ class Stk_Casino
         $this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_styles');
         $this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
         $this->loader->add_action('init', $plugin_public, 'register_custom_post_types');
-        //$this->loader->add_action('acf/init', $plugin_public, 'register_local_field_group');
+        $this->loader->add_action('acf/init', $plugin_public, 'register_local_field_group');
         $this->loader->add_action('init', $plugin_shortcodes, 'register_shortcodes');
         $this->loader->add_action('plugins_loaded', $plugin_activator, 'affiliate_tracker_install');
     }
